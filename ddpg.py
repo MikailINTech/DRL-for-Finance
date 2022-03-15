ad48b345a4f7cf6a68bf1bef424cf50c9ba0b71b
Mikail Duzenli <Duzenlimikail@gmail.com>
2022-03-15 13:41:15 +0100

    Speed up training with torch no grad

--- ddpg.py
+++ ddpg.py
@@ -16,7 +16,7 @@ from utils import *
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class DDPGagent:
-    def __init__(self, env, hidden_size=256, actor_learning_rate=1e-4, critic_learning_rate=1e-3, gamma=0.99, tau=1e-2, max_memory_size=50000):
+    def __init__(self, env, hidden_size=256, actor_learning_rate=1e-4, critic_learning_rate=1e-3, gamma=0.99, tau=0.995, max_memory_size=50000):
         # Params
         self.num_states = env.observation_space.shape[0]
         self.num_actions = env.action_space.shape[0]
@@ -40,7 +40,15 @@ class DDPGagent:
         self.critic_criterion  = nn.MSELoss()
         self.actor_optimizer  = optim.Adam(self.actor.parameters(), lr=actor_learning_rate)
         self.critic_optimizer = optim.Adam(self.critic.parameters(), lr=critic_learning_rate)
-    
+        
+        # Freeze target networks with respect to optimizers (only update via polyak averaging)
+        for par in self.actor_target.parameters():
+            par.requires_grad = False
+        for par in self.critic_target.parameters():
+            par.requires_grad = False
+        
+        
+            
     def get_action(self, state):
         state = Variable(torch.from_numpy(state).float().unsqueeze(0)).to(device)
         action = self.actor.forward(state)
@@ -49,33 +57,40 @@ class DDPGagent:
     
     def update(self, batch_size):
         states, actions, rewards, next_states, _ = self.memory.sample(batch_size)
+        
         states = torch.FloatTensor(states).to(device)
         actions = torch.FloatTensor(actions).to(device)
         rewards = torch.FloatTensor(rewards).to(device)
         next_states = torch.FloatTensor(next_states).to(device)
-    
-        # Critic loss        
-        Qvals = self.critic.forward(states, actions)
-        next_actions = self.actor_target.forward(next_states)
-        next_Q = self.critic_target.forward(next_states, next_actions.detach())
-        Qprime = rewards + self.gamma * next_Q
-        critic_loss = self.critic_criterion(Qvals, Qprime)
-
-        # Actor loss
-        policy_loss = -self.critic.forward(states, self.actor.forward(states)).mean()
         
-        # update networks
-        self.actor_optimizer.zero_grad()
-        policy_loss.backward()
-        self.actor_optimizer.step()
-
+        # Critic update 
         self.critic_optimizer.zero_grad()
+        Qvals = self.critic.forward(states, actions)
+        with torch.no_grad():
+            next_actions = self.actor_target.forward(next_states)
+            next_Q = self.critic_target.forward(next_states, next_actions.detach())
+            Qprime = rewards + self.gamma * next_Q
+        critic_loss = self.critic_criterion(Qvals, Qprime)
+        
         critic_loss.backward() 
         self.critic_optimizer.step()
+        
+        for p in self.critic.parameters():
+            p.requires_grad = False
+        
+        # Actor update
+        self.actor_optimizer.zero_grad()
+        policy_loss = -self.critic.forward(states, self.actor.forward(states)).mean()
+        policy_loss.backward()
+        self.actor_optimizer.step()
+        
+        for p in self.critic.parameters():
+            p.requires_grad = True
 
+        with torch.no_grad():
         # update target networks 
-        for target_param, param in zip(self.actor_target.parameters(), self.actor.parameters()):
-            target_param.data.copy_(param.data * self.tau + target_param.data * (1.0 - self.tau))
-       
-        for target_param, param in zip(self.critic_target.parameters(), self.critic.parameters()):
-            target_param.data.copy_(param.data * self.tau + target_param.data * (1.0 - self.tau))
+            for target_param, param in zip(self.actor_target.parameters(), self.actor.parameters()):
+                target_param.data.copy_(param.data * self.tau + target_param.data * (1.0 - self.tau))
+           
+            for target_param, param in zip(self.critic_target.parameters(), self.critic.parameters()):
+                target_param.data.copy_(param.data * self.tau + target_param.data * (1.0 - self.tau))
